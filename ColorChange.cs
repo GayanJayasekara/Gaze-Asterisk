a509df2e348fc13749c9f591b7dfeb916b60cf40
Alex <aspindle@ucsd.edu>
2018-10-01 10:46:26 -0700

    Add files via upload

---
+++ ColorChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour {
+
+	public Color colorStart = Color.cyan;
+	public Color colorEnd = Color.blue;
+	public float duration = 1.0F;
+	public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float lerp = Mathf.PingPong(Time.time, duration) / duration;
+		rend.material.color = Color.Lerp(colorStart, colorEnd, lerp);
+	}
+
+	/*void onCollisionEnter (Collision col) {
+		rend.material.color = Color.yellow;
+	}*/
+}
---
+++ Raycast.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour {
+
+	public Color colorStart = Color.red;
+	public Color colorEnd = Color.yellow;
+	public float duration = 1.0F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float lerp = Mathf.PingPong(Time.time, duration) / duration;
+
+
+		Ray ray = new Ray (transform.position, transform.forward);
+		RaycastHit hitInfo;
+
+
+		if (Physics.Raycast(ray, out hitInfo)) {
+			Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
+			//SetColor(hitInfo.collider.material, FF000A);
+			
+			
+
+			if(hitInfo.collider.gameObject.tag == "HitTag") {
+				print (hitInfo.collider.gameObject.name);
+				//hitInfo.collider.gameObject.rend.material.ChangeColor(Color.yellow);
+				hitInfo.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.Lerp(colorStart, colorEnd, lerp);
+				//hitInfo.collider.gameObject.rend.material.color = Color.yellow;
+			}
+
+		} else {
+			Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.green);
+		}
+
+	}
+
+
+}
+
+
+/*
+/// Changes the material attached to the gameObject
+public static class ChangeMaterial(GameObject go, Material mat)
+{
+    go.renderer.material = mat;
+}
+
+
+/// Changes the color of the material
+public static class ChangeColor(Material mat, Color color)
+{
+    mat.SetColor("_Color", color);              
+}
+*/
